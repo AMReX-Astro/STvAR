5a257d117e12e65533951356f0183a4775ca9afd
Donald E. Willcox <eugene.willcox@gmail.com>
2019-11-06 15:09:32 -0800

    Revise the FE Integrator to derive from IntegratorBase.

--- Source/AMReX_FEIntegrator.cpp
+++ Source/AMReX_FEIntegrator.cpp
@@ -2,13 +2,10 @@
 
 using namespace amrex;
 
-FEIntegrator::FEIntegrator(std::function<void(amrex::MultiFab&, amrex::MultiFab&, amrex::Real)> F,
+FEIntegrator::FEIntegrator(std::function<void(amrex::MultiFab&, const amrex::MultiFab&, const amrex::Real)> F,
                            amrex::MultiFab& S_old_external, 
                            amrex::MultiFab& S_new_external, 
-                           amrex::Real initial_time) : Fun(F),
-                                                       time(initial_time),
-                                                       S_old(S_old_external),
-                                                       S_new(S_new_external)
+                           amrex::Real initial_time) : IntegratorBase(F, S_old_external, S_new_external, initial_time)
 {
     // Create temporary MultiFab
     F_tmp_ptr = std::make_unique<MultiFab>(S_old.boxArray(), S_old.DistributionMap(), S_old.nComp(), S_old.nGrow());
@@ -22,7 +19,7 @@ Real FEIntegrator::advance(const Real timestep)
 
     // F_tmp = RHS(S_old, t_old)
     MultiFab& F_tmp = *F_tmp_ptr;
-    Fun(F_tmp, S_old, time);
+    rhs(F_tmp, S_old, time);
 
     // S_new += timestep * dS/dt
     MultiFab::Saxpy(S_new, timestep, F_tmp, 0, 0, S_new.nComp(), S_new.nGrow());
@@ -32,19 +29,4 @@ Real FEIntegrator::advance(const Real timestep)
 
     // Return timestep
     return timestep;
-}
-
-amrex::MultiFab& FEIntegrator::get_new_data()
-{
-    return S_new;
-}
-
-amrex::MultiFab& FEIntegrator::get_old_data()
-{
-    return S_old;
-}
-
-amrex::Real FEIntegrator::get_time()
-{
-    return time;
 }
