e8a60940980357668781bac30540991aec244139
Adam Peterson <ajpeterson@lbl.gov>
2020-02-04 12:14:05 -0800

    Improved pow and det calculation

--- Source/post_update.cpp
+++ Source/post_update.cpp
@@ -20,12 +20,13 @@ void rescale_state (MultiFab& state_mf)
     {
 	// for example:
 	amrex::Real det = state_fab(i, j, k, Idx::gbar00)*state_fab(i, j, k, Idx::gbar11)*state_fab(i, j, k, Idx::gbar22) - state_fab(i, j, k, Idx::gbar00)*std::pow(state_fab(i, j, k, Idx::gbar12), 2) - std::pow(state_fab(i, j, k, Idx::gbar01), 2)*state_fab(i, j, k, Idx::gbar22) + 2*state_fab(i, j, k, Idx::gbar01)*state_fab(i, j, k, Idx::gbar02)*state_fab(i, j, k, Idx::gbar12) - std::pow(state_fab(i, j, k, Idx::gbar02), 2)*state_fab(i, j, k, Idx::gbar11);
-	state_fab(i, j, k, Idx::gbar00) = state_fab(i, j, k, Idx::gbar00) * std::pow(det,-1/3);
-	state_fab(i, j, k, Idx::gbar01) = state_fab(i, j, k, Idx::gbar01) * std::pow(det,-1/3);
-	state_fab(i, j, k, Idx::gbar02) = state_fab(i, j, k, Idx::gbar02) * std::pow(det,-1/3);
-	state_fab(i, j, k, Idx::gbar11) = state_fab(i, j, k, Idx::gbar11) * std::pow(det,-1/3);
-	state_fab(i, j, k, Idx::gbar12) = state_fab(i, j, k, Idx::gbar12) * std::pow(det,-1/3);
-	state_fab(i, j, k, Idx::gbar22) = state_fab(i, j, k, Idx::gbar22) * std::pow(det,-1/3);
+	amrex::Real scale_factor = 1.0/std::pow(det,1.0/3.0);
+	state_fab(i, j, k, Idx::gbar00) = state_fab(i, j, k, Idx::gbar00) * scale_factor;
+	state_fab(i, j, k, Idx::gbar01) = state_fab(i, j, k, Idx::gbar01) * scale_factor;
+	state_fab(i, j, k, Idx::gbar02) = state_fab(i, j, k, Idx::gbar02) * scale_factor;
+	state_fab(i, j, k, Idx::gbar11) = state_fab(i, j, k, Idx::gbar11) * scale_factor;
+	state_fab(i, j, k, Idx::gbar12) = state_fab(i, j, k, Idx::gbar12) * scale_factor;
+	state_fab(i, j, k, Idx::gbar22) = state_fab(i, j, k, Idx::gbar22) * scale_factor;
     });
   }
 }
