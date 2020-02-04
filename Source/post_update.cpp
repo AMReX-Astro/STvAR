0f4a107a470beb389ba2220dc9023ffd3139c605
Adam Peterson <ajpeterson@lbl.gov>
2020-02-04 11:12:30 -0800

    Added formula for detg

--- Source/post_update.cpp
+++ Source/post_update.cpp
@@ -19,8 +19,13 @@ void rescale_state (MultiFab& state_mf)
     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
 	// for example:
-	amrex::Real det = [determinant];
-	state_fab(i, j, k, gamma_00) = state_fab(i, j, k, gamma_00) * det;
+	amrex::Real det = state_fab(i, j, k, Idx::gbar00)*state_fab(i, j, k, Idx::gbar11)*state_fab(i, j, k, Idx::gbar22) - state_fab(i, j, k, Idx::gbar00)*std::pow(state_fab(i, j, k, Idx::gbar12), 2) - std::pow(state_fab(i, j, k, Idx::gbar01), 2)*state_fab(i, j, k, Idx::gbar22) + 2*state_fab(i, j, k, Idx::gbar01)*state_fab(i, j, k, Idx::gbar02)*state_fab(i, j, k, Idx::gbar12) - std::pow(state_fab(i, j, k, Idx::gbar02), 2)*state_fab(i, j, k, Idx::gbar11);
+	state_fab(i, j, k, Idx::gbar00) = state_fab(i, j, k, Idx::gbar00) * std::pow(det,-1/3);
+	state_fab(i, j, k, Idx::gbar01) = state_fab(i, j, k, Idx::gbar01) * std::pow(det,-1/3);
+	state_fab(i, j, k, Idx::gbar02) = state_fab(i, j, k, Idx::gbar02) * std::pow(det,-1/3);
+	state_fab(i, j, k, Idx::gbar11) = state_fab(i, j, k, Idx::gbar11) * std::pow(det,-1/3);
+	state_fab(i, j, k, Idx::gbar12) = state_fab(i, j, k, Idx::gbar12) * std::pow(det,-1/3);
+	state_fab(i, j, k, Idx::gbar22) = state_fab(i, j, k, Idx::gbar22) * std::pow(det,-1/3);
     });
   }
 }
