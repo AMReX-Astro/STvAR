71c39ef4e6838551c69688db81b8c930253cc894
Adam Peterson <ajpeterson@lbl.gov>
2020-03-18 13:58:51 -0700

    Made consistent with new variable names

--- Source/post_update.cpp
+++ Source/post_update.cpp
@@ -19,14 +19,16 @@ void rescale_state (MultiFab& state_mf)
     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
 	// for example:
-	amrex::Real det = state_fab(i, j, k, Idx::gbar00)*state_fab(i, j, k, Idx::gbar11)*state_fab(i, j, k, Idx::gbar22) - state_fab(i, j, k, Idx::gbar00)*std::pow(state_fab(i, j, k, Idx::gbar12), 2) - std::pow(state_fab(i, j, k, Idx::gbar01), 2)*state_fab(i, j, k, Idx::gbar22) + 2*state_fab(i, j, k, Idx::gbar01)*state_fab(i, j, k, Idx::gbar02)*state_fab(i, j, k, Idx::gbar12) - std::pow(state_fab(i, j, k, Idx::gbar02), 2)*state_fab(i, j, k, Idx::gbar11);
+	amrex::Real det = state_fab(i, j, k, Idx::gambar00)*state_fab(i, j, k, Idx::gambar11)*state_fab(i, j, k, Idx::gambar22) - state_fab(i, j, k, Idx::gambar00)*std::pow(state_fab(i, j, k, Idx::gambar12), 2) - std::pow(state_fab(i, j, k, Idx::gambar01), 2)*state_fab(i, j, k, Idx::gambar22) + 2*state_fab(i, j, k, Idx::gambar01)*state_fab(i, j, k, Idx::gambar02)*state_fab(i, j, k, Idx::gambar12) - std::pow(state_fab(i, j, k, Idx::gambar02), 2)*state_fab(i, j, k, Idx::gambar11);
 	amrex::Real scale_factor = 1.0/std::pow(det,1.0/3.0);
-	state_fab(i, j, k, Idx::gbar00) = state_fab(i, j, k, Idx::gbar00) * scale_factor;
-	state_fab(i, j, k, Idx::gbar01) = state_fab(i, j, k, Idx::gbar01) * scale_factor;
-	state_fab(i, j, k, Idx::gbar02) = state_fab(i, j, k, Idx::gbar02) * scale_factor;
-	state_fab(i, j, k, Idx::gbar11) = state_fab(i, j, k, Idx::gbar11) * scale_factor;
-	state_fab(i, j, k, Idx::gbar12) = state_fab(i, j, k, Idx::gbar12) * scale_factor;
-	state_fab(i, j, k, Idx::gbar22) = state_fab(i, j, k, Idx::gbar22) * scale_factor;
+	
+	state_fab(i, j, k, Idx::gambar00) = state_fab(i, j, k, Idx::gambar00) * scale_factor;
+	state_fab(i, j, k, Idx::gambar01) = state_fab(i, j, k, Idx::gambar01) * scale_factor;
+	state_fab(i, j, k, Idx::gambar02) = state_fab(i, j, k, Idx::gambar02) * scale_factor;
+	state_fab(i, j, k, Idx::gambar11) = state_fab(i, j, k, Idx::gambar11) * scale_factor;
+	state_fab(i, j, k, Idx::gambar12) = state_fab(i, j, k, Idx::gambar12) * scale_factor;
+	state_fab(i, j, k, Idx::gambar22) = state_fab(i, j, k, Idx::gambar22) * scale_factor;
+	
     });
   }
 }
