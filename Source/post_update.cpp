ecd0a1f6eca97f37962128688fcd783dbe6672cc
Adam Peterson <ajpeterson@lbl.gov>
2020-04-28 15:00:36 -0700

    Adjusted for new solver

--- Source/post_update.cpp
+++ Source/post_update.cpp
@@ -19,54 +19,64 @@ void rescale_state (MultiFab& state_mf)
     [=] AMREX_GPU_DEVICE (int i, int j, int k) noexcept
     {
 	// for example:
-	amrex::Real det = state_fab(i, j, k, Idx::gambar00)*state_fab(i, j, k, Idx::gambar11)*state_fab(i, j, k, Idx::gambar22) - state_fab(i, j, k, Idx::gambar00)*std::pow(state_fab(i, j, k, Idx::gambar12), 2) - std::pow(state_fab(i, j, k, Idx::gambar01), 2)*state_fab(i, j, k, Idx::gambar22) + 2*state_fab(i, j, k, Idx::gambar01)*state_fab(i, j, k, Idx::gambar02)*state_fab(i, j, k, Idx::gambar12) - std::pow(state_fab(i, j, k, Idx::gambar02), 2)*state_fab(i, j, k, Idx::gambar11);
-	amrex::Real scale_factor = 1.0/std::pow(det,1.0/3.0);
-	
-	state_fab(i, j, k, Idx::gambar00) = state_fab(i, j, k, Idx::gambar00) * scale_factor;
-	state_fab(i, j, k, Idx::gambar01) = state_fab(i, j, k, Idx::gambar01) * scale_factor;
-	state_fab(i, j, k, Idx::gambar02) = state_fab(i, j, k, Idx::gambar02) * scale_factor;
-	state_fab(i, j, k, Idx::gambar11) = state_fab(i, j, k, Idx::gambar11) * scale_factor;
-	state_fab(i, j, k, Idx::gambar12) = state_fab(i, j, k, Idx::gambar12) * scale_factor;
-	state_fab(i, j, k, Idx::gambar22) = state_fab(i, j, k, Idx::gambar22) * scale_factor;
+        
+        amrex::Real gamtildeLL00 = state_fab(i, j, k, Idx::gamtildeLL00);
+        amrex::Real gamtildeLL01 = state_fab(i, j, k, Idx::gamtildeLL01);
+        amrex::Real gamtildeLL02 = state_fab(i, j, k, Idx::gamtildeLL02);
+        amrex::Real gamtildeLL10 = state_fab(i, j, k, Idx::gamtildeLL01);
+        amrex::Real gamtildeLL11 = state_fab(i, j, k, Idx::gamtildeLL11);
+        amrex::Real gamtildeLL12 = state_fab(i, j, k, Idx::gamtildeLL12);
+        amrex::Real gamtildeLL20 = state_fab(i, j, k, Idx::gamtildeLL02);
+        amrex::Real gamtildeLL21 = state_fab(i, j, k, Idx::gamtildeLL12);
+        amrex::Real gamtildeLL22 = state_fab(i, j, k, Idx::gamtildeLL22);	
+        
+        amrex::Real det = gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20;
+            
+        amrex::Real scale_factor = 1.0/std::pow(det,1.0/3.0);
+        
+        state_fab(i, j, k, Idx::gamtildeLL00) = gamtildeLL00 * scale_factor;
+        state_fab(i, j, k, Idx::gamtildeLL01) = gamtildeLL01 * scale_factor;
+        state_fab(i, j, k, Idx::gamtildeLL02) = gamtildeLL02 * scale_factor;
+        state_fab(i, j, k, Idx::gamtildeLL11) = gamtildeLL11 * scale_factor;
+        state_fab(i, j, k, Idx::gamtildeLL12) = gamtildeLL12 * scale_factor;
+        state_fab(i, j, k, Idx::gamtildeLL22) = gamtildeLL22 * scale_factor;
 
-	amrex::Real gambar00 = state_fab(i, j, k, Idx::gambar00);
-        amrex::Real gambar01 = state_fab(i, j, k, Idx::gambar01);
-        amrex::Real gambar02 = state_fab(i, j, k, Idx::gambar02);
-        amrex::Real gambar10 = state_fab(i, j, k, Idx::gambar01);
-        amrex::Real gambar11 = state_fab(i, j, k, Idx::gambar11);
-        amrex::Real gambar12 = state_fab(i, j, k, Idx::gambar12);
-        amrex::Real gambar20 = state_fab(i, j, k, Idx::gambar02);
-        amrex::Real gambar21 = state_fab(i, j, k, Idx::gambar12);
-        amrex::Real gambar22 = state_fab(i, j, k, Idx::gambar22);	
+        amrex::Real AtildeLL00 = state_fab(i, j, k, Idx::AtildeLL00);
+        amrex::Real AtildeLL01 = state_fab(i, j, k, Idx::AtildeLL01);
+        amrex::Real AtildeLL02 = state_fab(i, j, k, Idx::AtildeLL02);
+        amrex::Real AtildeLL10 = state_fab(i, j, k, Idx::AtildeLL01);
+        amrex::Real AtildeLL11 = state_fab(i, j, k, Idx::AtildeLL11);
+        amrex::Real AtildeLL12 = state_fab(i, j, k, Idx::AtildeLL12);
+        amrex::Real AtildeLL20 = state_fab(i, j, k, Idx::AtildeLL02);
+        amrex::Real AtildeLL21 = state_fab(i, j, k, Idx::AtildeLL12);
+        amrex::Real AtildeLL22 = state_fab(i, j, k, Idx::AtildeLL22);
 
-	amrex::Real Abar00 = state_fab(i, j, k, Idx::Abar00);
-        amrex::Real Abar01 = state_fab(i, j, k, Idx::Abar01);
-        amrex::Real Abar02 = state_fab(i, j, k, Idx::Abar02);
-        amrex::Real Abar10 = state_fab(i, j, k, Idx::Abar01);
-        amrex::Real Abar11 = state_fab(i, j, k, Idx::Abar11);
-        amrex::Real Abar12 = state_fab(i, j, k, Idx::Abar12);
-        amrex::Real Abar20 = state_fab(i, j, k, Idx::Abar02);
-        amrex::Real Abar21 = state_fab(i, j, k, Idx::Abar12);
-        amrex::Real Abar22 = state_fab(i, j, k, Idx::Abar22);
+        amrex::Real gamtildeUU00 = (gamtildeLL11*gamtildeLL22 - gamtildeLL12*gamtildeLL21)/(gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20);
+        
+        amrex::Real gamtildeUU01 = (-gamtildeLL01*gamtildeLL22 + gamtildeLL02*gamtildeLL21)/(gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20);
+        
+        amrex::Real gamtildeUU02 = (gamtildeLL01*gamtildeLL12 - gamtildeLL02*gamtildeLL11)/(gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20);
 
-	amrex::Real gambarinv00 = (gambar11*gambar22 - gambar12*gambar21)/(gambar00*gambar11*gambar22 - gambar00*gambar12*gambar21 - gambar01*gambar10*gambar22 + gambar01*gambar12*gambar20 + gambar02*gambar10*gambar21 - gambar02*gambar11*gambar20);
-        amrex::Real gambarinv01 = (-gambar01*gambar22 + gambar02*gambar21)/(gambar00*gambar11*gambar22 - gambar00*gambar12*gambar21 - gambar01*gambar10*gambar22 + gambar01*gambar12*gambar20 + gambar02*gambar10*gambar21 - gambar02*gambar11*gambar20);
-        amrex::Real gambarinv02 = (gambar01*gambar12 - gambar02*gambar11)/(gambar00*gambar11*gambar22 - gambar00*gambar12*gambar21 - gambar01*gambar10*gambar22 + gambar01*gambar12*gambar20 + gambar02*gambar10*gambar21 - gambar02*gambar11*gambar20);
-        amrex::Real gambarinv10 = (-gambar10*gambar22 + gambar12*gambar20)/(gambar00*gambar11*gambar22 - gambar00*gambar12*gambar21 - gambar01*gambar10*gambar22 + gambar01*gambar12*gambar20 + gambar02*gambar10*gambar21 - gambar02*gambar11*gambar20);
-        amrex::Real gambarinv11 = gambar00*(gambar00*gambar22 - gambar02*gambar20)/((gambar00*gambar11 - gambar01*gambar10)*(gambar00*gambar22 - gambar02*gambar20) - (gambar00*gambar12 - gambar02*gambar10)*(gambar00*gambar21 - gambar01*gambar20));
-        amrex::Real gambarinv12 = -gambar00*(gambar00*gambar12 - gambar02*gambar10)/((gambar00*gambar11 - gambar01*gambar10)*(gambar00*gambar22 - gambar02*gambar20) - (gambar00*gambar12 - gambar02*gambar10)*(gambar00*gambar21 - gambar01*gambar20));
-        amrex::Real gambarinv20 = (gambar10*gambar21 - gambar11*gambar20)/(gambar00*gambar11*gambar22 - gambar00*gambar12*gambar21 - gambar01*gambar10*gambar22 + gambar01*gambar12*gambar20 + gambar02*gambar10*gambar21 - gambar02*gambar11*gambar20);
-        amrex::Real gambarinv21 = -gambar00*(gambar00*gambar21 - gambar01*gambar20)/((gambar00*gambar11 - gambar01*gambar10)*(gambar00*gambar22 - gambar02*gambar20) - (gambar00*gambar12 - gambar02*gambar10)*(gambar00*gambar21 - gambar01*gambar20));
-        amrex::Real gambarinv22 = gambar00*(gambar00*gambar11 - gambar01*gambar10)/((gambar00*gambar11 - gambar01*gambar10)*(gambar00*gambar22 - gambar02*gambar20) - (gambar00*gambar12 - gambar02*gambar10)*(gambar00*gambar21 - gambar01*gambar20));
+        amrex::Real gamtildeUU10 = (-gamtildeLL10*gamtildeLL22 + gamtildeLL12*gamtildeLL20)/(gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20);
+        
+        amrex::Real gamtildeUU11 = gamtildeLL00*(gamtildeLL00*gamtildeLL22 - gamtildeLL02*gamtildeLL20)/((gamtildeLL00*gamtildeLL11 - gamtildeLL01*gamtildeLL10)*(gamtildeLL00*gamtildeLL22 - gamtildeLL02*gamtildeLL20) - (gamtildeLL00*gamtildeLL12 - gamtildeLL02*gamtildeLL10)*(gamtildeLL00*gamtildeLL21 - gamtildeLL01*gamtildeLL20));
+        
+        amrex::Real gamtildeUU12 = -gamtildeLL00*(gamtildeLL00*gamtildeLL12 - gamtildeLL02*gamtildeLL10)/((gamtildeLL00*gamtildeLL11 - gamtildeLL01*gamtildeLL10)*(gamtildeLL00*gamtildeLL22 - gamtildeLL02*gamtildeLL20) - (gamtildeLL00*gamtildeLL12 - gamtildeLL02*gamtildeLL10)*(gamtildeLL00*gamtildeLL21 - gamtildeLL01*gamtildeLL20));
+        
+        amrex::Real gamtildeUU20 = (gamtildeLL10*gamtildeLL21 - gamtildeLL11*gamtildeLL20)/(gamtildeLL00*gamtildeLL11*gamtildeLL22 - gamtildeLL00*gamtildeLL12*gamtildeLL21 - gamtildeLL01*gamtildeLL10*gamtildeLL22 + gamtildeLL01*gamtildeLL12*gamtildeLL20 + gamtildeLL02*gamtildeLL10*gamtildeLL21 - gamtildeLL02*gamtildeLL11*gamtildeLL20);
+        
+        amrex::Real gamtildeUU21 = -gamtildeLL00*(gamtildeLL00*gamtildeLL21 - gamtildeLL01*gamtildeLL20)/((gamtildeLL00*gamtildeLL11 - gamtildeLL01*gamtildeLL10)*(gamtildeLL00*gamtildeLL22 - gamtildeLL02*gamtildeLL20) - (gamtildeLL00*gamtildeLL12 - gamtildeLL02*gamtildeLL10)*(gamtildeLL00*gamtildeLL21 - gamtildeLL01*gamtildeLL20));
+        
+        amrex::Real gamtildeUU22 = gamtildeLL00*(gamtildeLL00*gamtildeLL11 - gamtildeLL01*gamtildeLL10)/((gamtildeLL00*gamtildeLL11 - gamtildeLL01*gamtildeLL10)*(gamtildeLL00*gamtildeLL22 - gamtildeLL02*gamtildeLL20) - (gamtildeLL00*gamtildeLL12 - gamtildeLL02*gamtildeLL10)*(gamtildeLL00*gamtildeLL21 - gamtildeLL01*gamtildeLL20));
     
-	amrex::Real TrAbar = Abar00*gambarinv00 + Abar01*gambarinv01 + Abar02*gambarinv02 + Abar10*gambarinv10 + Abar11*gambarinv11 + Abar12*gambarinv12 + Abar20*gambarinv20 + Abar21*gambarinv21 + Abar22*gambarinv22;
+        amrex::Real TrAtilde = 0 + AtildeLL00*gamtildeUU00 + AtildeLL01*gamtildeUU01 + AtildeLL02*gamtildeUU02 + AtildeLL10*gamtildeUU10 + AtildeLL11*gamtildeUU11 + AtildeLL12*gamtildeUU12 + AtildeLL20*gamtildeUU20 + AtildeLL21*gamtildeUU21 + AtildeLL22*gamtildeUU22;
 
-	state_fab(i, j, k, Idx::Abar00) = Abar00 - 1.0/3.0*gambar00*TrAbar;
-	state_fab(i, j, k, Idx::Abar01) = Abar01 - 1.0/3.0*gambar01*TrAbar;
-	state_fab(i, j, k, Idx::Abar02) = Abar02 - 1.0/3.0*gambar02*TrAbar;
-	state_fab(i, j, k, Idx::Abar11) = Abar11 - 1.0/3.0*gambar11*TrAbar;
-	state_fab(i, j, k, Idx::Abar12) = Abar12 - 1.0/3.0*gambar12*TrAbar;
-	state_fab(i, j, k, Idx::Abar22) = Abar22 - 1.0/3.0*gambar22*TrAbar;
+	state_fab(i, j, k, Idx::AtildeLL00) = AtildeLL00 - 1.0/3.0*gamtildeLL00*TrAtilde;
+	state_fab(i, j, k, Idx::AtildeLL01) = AtildeLL01 - 1.0/3.0*gamtildeLL01*TrAtilde;
+	state_fab(i, j, k, Idx::AtildeLL02) = AtildeLL02 - 1.0/3.0*gamtildeLL02*TrAtilde;
+	state_fab(i, j, k, Idx::AtildeLL11) = AtildeLL11 - 1.0/3.0*gamtildeLL11*TrAtilde;
+	state_fab(i, j, k, Idx::AtildeLL12) = AtildeLL12 - 1.0/3.0*gamtildeLL12*TrAtilde;
+	state_fab(i, j, k, Idx::AtildeLL22) = AtildeLL22 - 1.0/3.0*gamtildeLL22*TrAtilde;
 
 	});
   }
