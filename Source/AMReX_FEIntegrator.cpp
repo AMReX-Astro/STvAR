3e6f627dd367ead9d51fbd12c887cdfcadea95af
Donald E. Willcox <eugene.willcox@gmail.com>
2020-06-10 14:07:25 -0700

    Change how the integrator expects valid data to enter. The integrator integrate() and advance() will always fill new data, assuming the old data is valid at the time passed in the Integrator constructor.

--- Source/AMReX_FEIntegrator.cpp
+++ Source/AMReX_FEIntegrator.cpp
@@ -12,9 +12,9 @@ FEIntegrator::FEIntegrator(amrex::MultiFab& S_old_external,
 
 Real FEIntegrator::advance(const Real timestep)
 {
-    // Assume before advance() that S_new is valid data at the current time ("time" argument)
-    // So we update S_old by copying the current state.
-    MultiFab::Copy(S_old, S_new, 0, 0, S_old.nComp(), S_old.nGrow());
+    // Assume before advance() that S_old is valid data at the current time ("time" argument)
+    // So first, copy S_new = S_old, since we will next do S_new += timestep * dS/dt
+    MultiFab::Copy(S_new, S_old, 0, 0, S_old.nComp(), S_old.nGrow());
 
     // F_tmp = RHS(S_old, t_old)
     MultiFab& F_tmp = *F_tmp_ptr;
