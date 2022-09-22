3ed102b68fe28c202399e54609833f4d52400d36
APeterson87 <ajpeterson@lbl.gov>
2022-09-22 15:37:31 -0700

    inputs files and slurm scripts for perlmutter scaling

---
+++ Exec/Z4c2/3gpu/slurm.3gpu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+## specify your allocation (with the _g) and that you want GPU nodes
+#SBATCH -A m4106_g
+#SBATCH -C gpu
+
+## the job will be named "Z4c2" in the queue and will save stdout to z4c2_[job ID].out
+#SBATCH -J Z4c2
+#SBATCH -o z4c2_%j.out
+
+## set the max walltime
+#SBATCH -t 2:00
+
+## specify the number of nodes you want
+#SBATCH -N 1
+
+## we use the same number of MPI ranks per node as GPUs per node
+#SBATCH --ntasks-per-node=4
+
+## assign 1 MPI rank per GPU on each node
+#SBATCH --gpus-per-task=1
+#SBATCH --gpu-bind=map_gpu:0,1,2,3
+
+# the -n argument is (--ntasks-per-node) * (-N) = (number of MPI ranks per node) * (number of nodes)
+srun -n 3 ./main3d.gnu.MPI.CUDA.ex inputs
